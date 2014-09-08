d4bbd00f1ad13a63591ef7e44f88869de72bd0f5
C.Wang <wcg0304@sina.com>
2014-09-08 23:20:19 +1000

    add toSeconds

--- src/main/webapp/resources/assets/js/markannotation.js
+++ src/main/webapp/resources/assets/js/markannotation.js
@@ -2,17 +2,27 @@
  * 
  */
 String.prototype.toHHMMSS = function () {
-    var mills = this.match("\\.\\d{3}");
-    var sec_num = parseInt(this, 10); // don't forget the second param
-    var hours   = Math.floor(sec_num / 3600);
-    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+  var mills = this.match("\\.\\d{3}");
+  var sec_num = parseInt(this, 10); // don't forget the second param
+  var hours   = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    var time    = hours+':'+minutes+':'+seconds+mills;
-    return time;
+  if (hours   < 10) {hours   = "0"+hours;}
+  if (minutes < 10) {minutes = "0"+minutes;}
+  if (seconds < 10) {seconds = "0"+seconds;}
+  var time    = hours+':'+minutes+':'+seconds+mills;
+  return time;
+}
+
+String.prototype.toSeconds = function () {
+  var match = this.match("(\\d{2,}):(\\d{2}):(\\d{2})(\\.\\d{3})");
+  var hours = parseInt(match[1]);
+  var minutes = parseInt(match[2]);
+  var seconds = parseInt(match[3]);
+  var mills = match[4];
+  var time = (hours * 3600 + minutes * 60 + seconds).toString() + mills;
+  return time;
 }
 
 myPlayer = videojs("example_video_1");
@@ -21,21 +31,21 @@ endTimer = 0;
 $(document).ready(function(){
 	
 	$("#start-button").click(function(){
-		//alert("start");
+		// alert("start");
 
-		startTime = myPlayer.currentTime().toFixed(3).toString();
+		startTime = myPlayer.currentTime();
 		myPlayer.play();
 		$(this).addClass("disabled");
 		$("#end-button").removeClass("disabled");	
 	});
 	
 	$("#end-button").click(function(){
-		//alert("end");
-		endTime = myPlayer.currentTime().toFixed(3).toString();
-		//alert(startTime + " " + endTime);
+		// alert("end");
+		endTime = myPlayer.currentTime();
+		// alert(startTime + " " + endTime);
 		$(this).addClass("disabled");
 		$("#start-button").removeClass("disabled");
-		$('#result').append('<tr><td><input name="va[][startTime]" value="' + startTime.toHHMMSS() + '" disabled /></td><td><input name="va[][endTime]" value="' + endTime.toHHMMSS() + '" disabled /></td><td><button class="delete-button btn btn-xs btn-danger">delete</button></td></tr>');
+		$('#result').append('<tr><td><input name="va[][startTime]" value="' + startTime.toFixed(3).toString().toHHMMSS() + '" disabled /></td><td><input name="va[][endTime]" value="' + endTime.toFixed(3).toString().toHHMMSS() + '" disabled /></td><td><button class="delete-button btn btn-xs btn-danger">delete</button></td></tr>');
 		addDeleteButtonListener();
 	});
 	
@@ -47,9 +57,9 @@ $(document).ready(function(){
 
 function addDeleteButtonListener() {
 	$('.delete-button').each(function(){
-		//alert("each");
+		// alert("each");
 		$(this).click(function(){
-			//alert("delete");
+			// alert("delete");
 			$(this).parent().parent().remove();
 		});
 	});
