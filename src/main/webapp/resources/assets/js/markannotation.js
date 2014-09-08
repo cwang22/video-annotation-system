724af28965a85d68dabe29d2427f0cc1dc24229b
C.Wang <wcg0304@sina.com>
2014-09-08 23:01:40 +1000

    format annotation time to hh:mm:ss.xxx

--- src/main/webapp/resources/assets/js/markannotation.js
+++ src/main/webapp/resources/assets/js/markannotation.js
@@ -1,7 +1,19 @@
 /**
  * 
  */
+String.prototype.toHHMMSS = function () {
+    var mills = this.match("\\.\\d{3}");
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    var time    = hours+':'+minutes+':'+seconds+mills;
+    return time;
+}
 
 myPlayer = videojs("example_video_1");
 startTime = 0;
@@ -11,7 +23,7 @@ $(document).ready(function(){
 	$("#start-button").click(function(){
 		//alert("start");
 
-		startTime = myPlayer.currentTime();
+		startTime = myPlayer.currentTime().toFixed(3).toString();
 		myPlayer.play();
 		$(this).addClass("disabled");
 		$("#end-button").removeClass("disabled");	
@@ -19,11 +31,11 @@ $(document).ready(function(){
 	
 	$("#end-button").click(function(){
 		//alert("end");
-		endTime = myPlayer.currentTime();
+		endTime = myPlayer.currentTime().toFixed(3).toString();
 		//alert(startTime + " " + endTime);
 		$(this).addClass("disabled");
 		$("#start-button").removeClass("disabled");
-		$('#result').append('<tr><td><input name="va[][startTime]" value="' + startTime + '" /></td><td><input name="va[][endTime]" value="' + endTime + '"/></td><td><button class="delete-button btn btn-xs btn-danger">delete</button></td></tr>');
+		$('#result').append('<tr><td><input name="va[][startTime]" value="' + startTime.toHHMMSS() + '" disabled /></td><td><input name="va[][endTime]" value="' + endTime.toHHMMSS() + '" disabled /></td><td><button class="delete-button btn btn-xs btn-danger">delete</button></td></tr>');
 		addDeleteButtonListener();
 	});
 	
