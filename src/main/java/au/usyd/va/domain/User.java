1b265207e3430710df51193dd791ce560e90408b
C.Wang <wcg0304@sina.com>
2014-09-13 20:07:44 +1000

    add user class

---
+++ src/main/java/au/usyd/va/domain/User.java
@@ -0,0 +1,77 @@
+package au.usyd.va.domain;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails {
+  
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -3904938781686891076L;
+  
+  private long id;
+  private String username;
+  private String password;
+  private boolean enabled;  
+  private boolean accountNonExpired;  
+  private boolean accountNonLocked;  
+  private boolean credentialsNonExpired;  
+  private Collection<GrantedAuthority>  authorities;
+  
+  public long getId() {
+    return id;
+  }
+  public void setId(long id) {
+    this.id = id;
+  }
+  public String getUsername() {
+    return username;
+  }
+  public void setUsername(String username) {
+    this.username = username;
+  }
+  public String getPassword() {
+    return password;
+  }
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  public boolean isEnabled() {
+    return enabled;
+  }
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
+  }
+  public boolean isAccountNonExpired() {
+    return accountNonExpired;
+  }
+  public void setAccountNonExpired(boolean accountNonExpired) {
+    this.accountNonExpired = accountNonExpired;
+  }
+  public boolean isAccountNonLocked() {
+    return accountNonLocked;
+  }
+  public void setAccountNonLocked(boolean accountNonLocked) {
+    this.accountNonLocked = accountNonLocked;
+  }
+  public boolean isCredentialsNonExpired() {
+    return credentialsNonExpired;
+  }
+  public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+    this.credentialsNonExpired = credentialsNonExpired;
+  }
+  public Collection<GrantedAuthority> getAuthorities() {
+    return authorities;
+  }
+  public void setAuthorities(Collection<GrantedAuthority> authorities) {
+    this.authorities = authorities;
+  }
+  public static long getSerialversionuid() {
+    return serialVersionUID;
+  }
+  
+  
+}
