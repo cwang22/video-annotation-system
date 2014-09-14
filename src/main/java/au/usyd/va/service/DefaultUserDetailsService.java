f7fe73fdbfaca65e29a0bef025c728e846271baf
C.Wang <wcg0304@sina.com>
2014-09-14 17:41:34 +1000

    add CURD methods for user service

--- src/main/java/au/usyd/va/service/DefaultUserDetailsService.java
+++ src/main/java/au/usyd/va/service/DefaultUserDetailsService.java
@@ -13,6 +13,8 @@ import org.springframework.security.core.userdetails.UserCache;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import au.usyd.va.domain.User;
 
@@ -21,42 +23,49 @@ import au.usyd.va.domain.User;
  * @author C
  * @TODO decouple UserDAO
  */
+@Service(value="DefaultUserDetailsService")
+@Transactional
 public class DefaultUserDetailsService implements UserDetailsService {
 
   protected final Log logger = LogFactory.getLog(getClass());
 
   @Autowired
   private SessionFactory sessionFactory;
-
-  @Autowired
-  private MessageSource messageSource;
-
-  @Autowired
-  private UserCache userCache;
+  
+  public void addUser(User user) {
+    this.sessionFactory.getCurrentSession().save(user);
+  }
+  
+  public void updateUser(User user) {
+    this.sessionFactory.getCurrentSession().merge(user);
+  }
+  
+  public void deleteUserByUsername(String username) {
+    User user = (User) this.sessionFactory.getCurrentSession().createQuery("FROM User where username =: username").setString("username", username)
+            .uniqueResult();
+    this.sessionFactory.getCurrentSession().delete(user);
+  }
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     Collection<GrantedAuthority> auths = new ArrayList<GrantedAuthority>();
-
-    User user = (User) this.userCache.getUserFromCache(username);
+    System.out.println("loadUserByUsername");
+    User user = this.sessionFactory.getCurrentSession().save();
 
     if (user == null) {
       user = (User) this.sessionFactory.getCurrentSession()
               .createQuery("FROM User where username =: username").setString("username", username)
               .uniqueResult();
       if (user == null)
-        throw new UsernameNotFoundException(this.messageSource.getMessage(
-                "UserDetailsService.userNotFount", new Object[] {username}, null));
+        throw new UsernameNotFoundException(
+                "UserDetailsService.userNotFount");
     }
 
     logger.info("*********************" + username + "***********************");
     logger.info(user.getAuthorities());
     logger.info("********************************************************");
 
-    this.userCache.putUserInCache(user);
-
     return user;
-
   }
 
 }
