1fd87d8b90e1a44ebb3dc4a343fa58b5f3d3390c
C.Wang <wcg0304@sina.com>
2014-09-15 16:54:05 +1000

    refactor frame domain

--- src/main/java/au/usyd/va/domain/VideoAnnotation.java
+++ src/main/java/au/usyd/va/domain/VideoAnnotation.java
@@ -1,10 +1,6 @@
 package au.usyd.va.domain;
 
-import java.util.List;
-
-import javax.persistence.CollectionTable;
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -31,17 +27,6 @@ public class VideoAnnotation {
   @Column(name = "Endtime")
   private double endTime;
 
-  @Column(name = "Keyframe")
-  private int keyFrame;
-
-  @Column(name = "Rank")
-  private int rank;
-  
-  @ElementCollection
-  @CollectionTable(name="Frame",joinColumns=@JoinColumn(name="videoAnnotationid"))
-  @Column(name = "Keyframes")
-  private List<Frame> keyFrames;
-
   public long getId() {
     return id;
   }
@@ -58,42 +43,19 @@ public class VideoAnnotation {
     this.video = video;
   }
 
-  public double getStarttime() {
+  public double getStartTime() {
     return startTime;
   }
 
-  public void setStarttime(double starttime) {
-    this.startTime = starttime;
+  public void setStartTime(double startTime) {
+    this.startTime = startTime;
   }
 
-  public double getEndtime() {
+  public double getEndTime() {
     return endTime;
   }
 
-  public void setEndtime(double endtime) {
-    this.endTime = endtime;
+  public void setEndTime(double endTime) {
+    this.endTime = endTime;
   }
-
-  public int getKeyFrame() {
-    return keyFrame;
-  }
-
-  public void setKeyFrame(int keyFrame) {
-    this.keyFrame = keyFrame;
-  }
-
-  public int getRank() {
-    return rank;
-  }
-
-  public void setRank(int rank) {
-    this.rank = rank;
-  }
-
-  @Override
-  public String toString() {
-    return "VideoAnnotation [id=" + id + ", video=" + video + ", starttime=" + startTime
-            + ", endtime=" + endTime + ", keyFrame=" + keyFrame + ", rank=" + rank + "]";
-  }
-
 }
