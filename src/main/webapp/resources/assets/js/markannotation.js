44a2695ea5cb7d0430147e6b321fe9eb3d66f244
C.Wang <wcg0304@sina.com>
2014-09-17 12:57:52 +1000

    add spin button next to segment input

--- src/main/webapp/resources/assets/js/markannotation.js
+++ src/main/webapp/resources/assets/js/markannotation.js
@@ -1,21 +1,27 @@
 /**
  * 
  */
-String.prototype.toHHMMSS = function () {
-  var mills = this.match("\\.\\d{3}");
+String.prototype.toHHMMSS = function() {
+  var mills = this.match("\\.\\d{1,3}");
   var sec_num = parseInt(this, 10); // don't forget the second param
-  var hours   = Math.floor(sec_num / 3600);
+  var hours = Math.floor(sec_num / 3600);
   var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
   var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-  if (hours   < 10) {hours   = "0"+hours;}
-  if (minutes < 10) {minutes = "0"+minutes;}
-  if (seconds < 10) {seconds = "0"+seconds;}
-  var time    = hours+':'+minutes+':'+seconds+mills;
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  var time = hours + ':' + minutes + ':' + seconds + mills;
   return time;
 }
 
-String.prototype.toSeconds = function () {
+String.prototype.toSeconds = function() {
   var match = this.match("(\\d{2,}):(\\d{2}):(\\d{2})(\\.\\d{3})");
   var hours = parseInt(match[1]);
   var minutes = parseInt(match[2]);
@@ -28,96 +34,151 @@ String.prototype.toSeconds = function () {
 myPlayer = videojs("example_video_1");
 startTime = 0;
 endTimer = 0;
-$(document).ready(function(){
-	
-	$("#start-button").click(function(){
-		// alert("start");
+$(document)
+        .ready(
+                function() {
 
-		startTime = myPlayer.currentTime();
-		myPlayer.play();
-		$(this).addClass("disabled");
-		$("#end-button").removeClass("disabled");	
-	});
-	
-	$("#end-button").click(function(){
-		// alert("end");
-		endTime = myPlayer.currentTime();
-		// alert(startTime + " " + endTime);
-		$(this).addClass("disabled");
-		$("#start-button").removeClass("disabled");
-		$('#result').append('<tr><td><input name="va[][startTime]" data-seconds="'+startTime.toFixed(3)+'" value="' + startTime.toFixed(3).toString().toHHMMSS() + '" disabled /></td><td><input name="va[][endTime]" data-seconds="'+endTime.toFixed(3)+'" value="' + endTime.toFixed(3).toString().toHHMMSS() + '" disabled /></td><td><button type="button" class="play-button btn btn-xs btn-primary">play</button>&nbsp;<button class="delete-button btn btn-xs btn-danger">delete</button></td></tr>');
-		addButtonListener();
-	});
-	
-	$("form").submit(function(){
-	  $(this).find('input[name^="va"]').removeAttr('disabled').each(function(){
-	    $(this).val($(this).attr('data-seconds'));
-	  });
-		$('form').append('<textarea class="hidden" name="json">' + JSON.stringify($("form").serializeJSON()) + '</textarea>');
-	});
-	
-	$("#prev-button").click(function(){
-	  myPlayer.currentTime(myPlayer.currentTime()-0.04);
-	});
-	
-	$("#next-button").click(function(){
-    myPlayer.currentTime(myPlayer.currentTime()+0.04);
-  });
-});
+                  $("#start-button").click(function() {
+                    // alert("start");
+
+                    startTime = myPlayer.currentTime();
+                    myPlayer.play();
+                    $(this).addClass("disabled");
+                    $("#end-button").removeClass("disabled");
+                  });
+
+                  $("#end-button")
+                          .click(
+                                  function() {
+                                    // alert("end");
+                                    endTime = myPlayer.currentTime();
+                                    // alert(startTime + " " + endTime);
+                                    $(this).addClass("disabled");
+                                    $("#start-button").removeClass("disabled");
+                                    $('#result')
+                                            .append(
+                                                    '<tr><td><div class="input-group spinner"><input name="va[][startTime]" class="form-control" data-seconds="'
+                                                            + startTime.toFixed(3)
+                                                            + '" value="'
+                                                            + startTime.toFixed(3).toString().toHHMMSS()
+                                                            + '" disabled /><div class="input-group-btn-vertical"><button class="btn btn-default" type="button"><i class="fa fa-caret-up"></i></button><button class="btn btn-default" type="button"><i class="fa fa-caret-down"></i></button></div></div></td><td><div class="input-group spinner"><input name="va[][endTime]" class="form-control"  data-seconds="'
+                                                            + endTime.toFixed(3)
+                                                            + '" value="'
+                                                            + endTime.toFixed(3).toString().toHHMMSS()
+                                                            + '" disabled /><div class="input-group-btn-vertical"><button class="btn btn-default" type="button"><i class="fa fa-caret-up"></i></button><button class="btn btn-default" type="button"><i class="fa fa-caret-down"></i></button></div></div></td><td><button type="button" class="play-button btn btn-xs btn-primary">play</button>&nbsp;<button class="delete-button btn btn-xs btn-danger">delete</button></td></tr>');
+                                    addButtonListener();
+                                  });
+
+                  $("form").submit(function() {
+                    $(this).find('input[name^="va"]').removeAttr('disabled').each(function() {
+                      $(this).val($(this).attr('data-seconds'));
+                    });
+                    $('form').append('<textarea class="hidden" name="json">' + JSON.stringify($("form").serializeJSON()) + '</textarea>');
+                  });
+                  
+                  
+
+                  $("#prev-button").click(function() {
+                    myPlayer.currentTime(myPlayer.currentTime() - 0.04);
+                  });
+
+                  $("#next-button").click(function() {
+                    myPlayer.currentTime(myPlayer.currentTime() + 0.04);
+                  });
+                });
 
 function addButtonListener() {
-	$('.delete-button').each(function(){
-		// alert("each");
-		$(this).click(function(e){
-		  e.preventDefault();
-			// alert("delete");
-			$(this).parent().parent().remove();
-		});
-	});
-	
-	$('.play-button').each(function(){
-    $(this).click(function(){
+  $('.delete-button').each(function() {
+    // alert("each");
+    $(this).click(function(e) {
+      e.preventDefault();
+      // alert("delete");
+      $(this).parent().parent().remove();
+    });
+  });
+
+  $('.play-button').each(function() {
+    $(this).click(function() {
       var startAt = $(this).parent().parent().children().children("input[name='va[][startTime]']").val().toSeconds();
-      var stopAt  = $(this).parent().parent().children().children("input[name='va[][endTime]']").val().toSeconds();
+      var stopAt = $(this).parent().parent().children().children("input[name='va[][endTime]']").val().toSeconds();
       myPlayer.currentTime(startAt).play();
-      
-      var pausePlayer = function(){
-        if(Math.abs( myPlayer.currentTime() - stopAt ) < 0.1){
+
+      var pausePlayer = function() {
+        if (Math.abs(myPlayer.currentTime() - stopAt) < 0.1) {
           myPlayer.pause();
-          myPlayer.off('timeupdate',pausePlayer);
+          myPlayer.off('timeupdate', pausePlayer);
         }
       };
-      
-      myPlayer.on('timeupdate',pausePlayer);
-      
-      });
+
+      myPlayer.on('timeupdate', pausePlayer);
+
     });
-	
-	$("#result tr").each(function(){
-	  $(this).hover(function(){
-	    console.log("hoveron");
-	    var startAt = $(this).children().children("input[name='va[][startTime]']").val().toSeconds();
-	    var endAt = $(this).children().children("input[name='va[][endTime]']").val().toSeconds();
-	    console.log(startAt + "," + endAt);
-	    myPlayer.markers({
-	      setting: {
-	        forceInitialization:true,
-	        markerStyle:{
-	          'width':'7px',
-	          'border-radius': '0',
-	          'background-color': 'white',
-	          'opcity': '15%'
-	        }
-	      },
-	      
-	      marker_breaks:[[startAt, endAt]],
-	      marker_text:["start","end"]
-	   });
-	  },function(){$(".vjs-marker").remove();});
-	});
-	
+  });
+  
+  $(".spinner .btn:first-of-type").each(function(){
+    var intervalReturn;
+    $(this).mousedown(function(){
+      var button = $(this);
+      intervalReturn = setInterval(function(){
+        var input = button.parents(".spinner").children("input");
+        var seconds = parseFloat(input.attr("data-seconds"));
+        console.log("seconds:"+seconds);
+        seconds +=0.04;
+        console.log("seconds:"+seconds);
+        input.attr("data-seconds",seconds)
+        input.val(seconds.toString().toHHMMSS());
+      },100);
+    });
+    
+    $(this).mouseup(function(){
+      clearInterval(intervalReturn);
+    });
+
+  });
+  
+  $(".spinner .btn:last-of-type").each(function(){
+    var intervalReturn;
+    $(this).mousedown(function(){
+      var button = $(this);
+      intervalReturn = setInterval(function(){
+        var input = button.parents(".spinner").children("input");
+        var seconds = parseFloat(input.attr("data-seconds"));
+        console.log("seconds:"+seconds);
+        seconds -=0.04;
+        console.log("seconds:"+seconds);
+        input.attr("data-seconds",seconds)
+        input.val(seconds.toString().toHHMMSS());
+      },100);
+    });
+    
+    $(this).mouseup(function(){
+      clearInterval(intervalReturn);
+    });
+
+  });
+
+  $("#result tr").each(function() {
+    $(this).hover(function() {
+      var startAt = $(this).children().children().children("input[name='va[][startTime]']").val().toSeconds();
+      var endAt = $(this).children().children().children("input[name='va[][endTime]']").val().toSeconds();
+      console.log(startAt + "," + endAt);
+      myPlayer.markers({
+        setting: {
+          forceInitialization: true,
+          markerStyle: {
+            'width': '7px',
+            'border-radius': '0',
+            'background-color': 'white',
+            'opcity': '15%'
+          }
+        },
+
+        marker_breaks: [[startAt, endAt]],
+        marker_text: ["start", "end"]
+      });
+    }, function() {
+      $(".vjs-marker").remove();
+    });
+  });
+
 }
-
-
-
-
