5e33daefce6e549c7c26470f602df57236ed170f
C.Wang <wcg0304@sina.com>
2014-10-13 22:49:52 +1100

    update timeline to support single frame segment

--- src/main/webapp/resources/assets/js/jquery.timeline.js
+++ src/main/webapp/resources/assets/js/jquery.timeline.js
@@ -11,9 +11,16 @@
               var _start = settings.start;
               var _end = settings.end;
               var _id = settings.id;
-              var _width = 100 * (_end - _start) / _duration;
-              var _pos = (_start / _duration) * 100;
-              var m = $("<div class=\"segment\" data-id=\""+_id+"\"></div>");
+              var _width, _pos, m;
+              _pos = (_start / _duration) * 100;
+              console.log(_end == null);
+              m = $("<div class=\"segment\" data-id=\""+_id+"\"></div>");
+              if(_end == null){
+                _width = 0.5;
+                m.addClass("object");
+              }else{
+                _width = 100 * (_end - _start) / _duration;
+              }
               m.css({"width":_width+'%',"left":_pos+'%'});
               $(this).append(m);
 
